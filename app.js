6d1022535032a57353af1a40cb4aa11db82a3bc7
melicarls <melissa@pillow.com>
2017-08-24 11:12:10 -0700

    Finish first draft

--- app.js
+++ app.js
@@ -1,20 +1,63 @@
-require('dotenv').config();
-var express = require('express');
-var request = require('request');
+require( 'dotenv' ).config();
+
+var express = require( 'express' );
+var request = require( 'request' );
+var moment = require( 'moment' );
 
 var app = express();
 
 var session_token = '';
+var target_restaurant = '';
 var target_schedule_id = '';
 
-app.get('/', function(req, res) {
-  res.send("Hello World!");
-});
+app.get('/', function( req, res ) {
+  res.send( "Hello World!" );
+} );
 
-app.listen(8888, function() {
-  console.log("Listening on port 8888");
-});
+app.listen( 8888, function() {
+  console.log( "Listening on port 8888" );
+  beginLoop();
+} );
 
+function beginLoop() {
+  setTimeout( function() {
+    console.log( "Doing a loop at " + moment() );
+
+    checkAndExecuteOrder();
+    beginLoop();
+
+  }, 5000 );
+}
+
+function checkAndExecuteOrder() {
+  if ( moment.get('hour') === '17' ) {
+    switch( moment().day() ) {
+      case 1: // Monday
+        target_restaurant = 'Sababa';
+
+      case 2: // Tuesday
+        target_restaurant = 'Sababa';
+
+      case 4: // Thursday
+        target_restaurant = 'Sababa';
+
+      case 5: // Friday
+        target_restaurant = 'Sababa';
+
+      default:
+        sleepUntilTomorrow();
+    }
+    submitLogin();
+    getMenu();
+    reserveMeal();
+  }
+}
+
+function sleepUntilTomorrow() {
+  setTimeout( function() {
+    console.log( "Sleeping for 23 hours" );
+  }, 3600000 )
+}
 
 function submitLogin() {
   var options = {
@@ -34,14 +77,14 @@ function submitLogin() {
     }
   };
 
-  request(options, function( error, response, body ) {
+  request( options, function( error, response, body ) {
     if ( !error && response.statusCode === 200 ) {
-      var data = JSON.parse(body);
+      var data = JSON.parse( body );
 
-      sesson_token = info.sessionToken.split(':')[1];
+      sesson_token = info.sessionToken.split( ':' )[1];
       return session_token;
     }
-  });
+  } );
 }
 
 function getMenu() {
@@ -61,12 +104,12 @@ function getMenu() {
     }
   };
 
-  request(options, function( error, response, body ) {
+  request( options, function( error, response, body ) {
     if ( !error && response.statusCode === 200 ) {
-      var data = JSON.parse(body);
+      var data = JSON.parse( body );
       target_schedule_id = getScheduleId( data );
     }
-  });
+  } );
 }
 
 function reserveMeal() {
@@ -79,8 +122,8 @@ function reserveMeal() {
       "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
       "Content-Type": "application/json;charset=UTF-8",
       "Referer": "https://secure." + process.env.SERVICE + ".com/main",
-      "Origin": "https://secure." + process.env.SERVICE + ".com"
-      // TODO: Add cookie
+      "Origin": "https://secure." + process.env.SERVICE + ".com",
+      "Cookie": "_mealpal_session=" + session_token + "; isLoggedIn=true;"
     },
     body: {
       pickup_time: "12:45-1:00pm",
@@ -89,25 +132,19 @@ function reserveMeal() {
       source: "web"
     }
   }
-}
 
+  request( options, function( error, response, body ) {
+    if ( !error && response.statusCode === 200 ) {
+      var data = JSON.parse( body );
+      console.log( data );
+    }
+  } );
+}
 
 function getScheduleId( menu_list ) {
-  menu_list.forEach( function( menu_option ) {
-    // TODO: Expand this to reserve different things on different days
-    if ( menu_option.restaurant.name === "Sababa" ) {
-      return menu_option.objectId;
+  for ( var i = 0; i < menu_list.length; i++ ) {
+    if ( menu_list[i].restaurant.name === target_restaurant ) {
+      return menu_list[i].objectId;
     }
-  });
+  }
 }
-
-// headers for post:
-    headers: {
-      "Accept": "application/json",
-      "Host": "secure." + process.env.SERVICE + ".com",
-      "Connection": "keep-alive",
-      "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
-      "Content-Type": "application/json;charset=UTF-8",
-      "Referer": "https://secure." + process.env.SERVICE + ".com/main",
-      "Origin": "https://secure." + process.env.SERVICE + ".com"
-    },
